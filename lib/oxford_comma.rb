86051b22bece11a948e780ef3fd93cf86ea9cf26
Travis Fox <travis.fox90@gmail.com>
2020-06-22 06:26:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size <= 1
+    return "#{array[0]}"
+  elsif array.size == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    last_element = array.pop
+    new_string = array.join(", ")
+    new_string << ", and #{last_element}"
+    return new_string
+  end
 end
